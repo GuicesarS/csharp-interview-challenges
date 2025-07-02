4e885475612dedb3b19bffcd27b449b14f3b9c0c
Guilherme Soares <guicesarsosil@gmail.com>
2025-07-02 12:48:08 -0300

    Filter users by city using JToken and LINQ expression

---
+++ JsonParsing/JObject_Linq/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using HttpClient client = new HttpClient();
+        var response = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Success");
+            string json = await response.Content.ReadAsStringAsync();
+            
+            // Convertion JToken
+            JToken token = JToken.Parse(json);
+
+            /* if (city == "South Christy")
+               Console.WriteLine($"{id} - {name} - {city}"); 
+            */
+            
+            var filter = token
+                .Where(x => x["address"]["city"].ToString() == "South Christy")
+                .ToList();
+            
+            foreach (JToken user in filter)
+            {
+                string id = user["id"].ToString();
+                string name = user["name"].ToString();
+                string city = user["address"]["city"].ToString();
+
+                Console.WriteLine($"Id:{id}, Name:{name}, City:{city}");
+            }
+        }
+        else
+        {   
+            string errorMessage = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Error");
+            Console.WriteLine(errorMessage);
+        }
+    }
+}
