3e9996668ecc16dcc3b0cc08e5a3191ef513342e
Guilherme Soares <guicesarsosil@gmail.com>
2025-07-02 11:28:14 -0300

    Get request example 2

---
+++ RestAPIs/GetAPI/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1;
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using HttpClient client = new HttpClient();
+        var responseClient = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+        if (responseClient.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Success");
+            
+            string json = await responseClient.Content.ReadAsStringAsync();
+            Console.WriteLine(json);
+        }
+        else
+        {
+            string errorMessage = await responseClient.Content.ReadAsStringAsync();
+            Console.WriteLine($"Error: {responseClient.StatusCode}");
+            Console.WriteLine(errorMessage);
+        }
+    }
+}
