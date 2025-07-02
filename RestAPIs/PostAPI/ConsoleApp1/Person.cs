0b58c6b3a78cd81877ef3b408b0440e0909449a9
Guilherme Soares <guicesarsosil@gmail.com>
2025-07-02 15:10:02 -0300

    Add example of HTTP POST using HttpClient with JSON serialization

---
+++ RestAPIs/PostAPI/ConsoleApp1/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1;
+
+public class Person
+{
+    public int UserId { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+}
---
+++ RestAPIs/PostAPI/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http.Json;
+using System.Text.Json;
+using ConsoleApp1;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var url = "https://jsonplaceholder.typicode.com/posts";
+        var jsonSerializerOptions = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
+        using (var httpClient = new HttpClient())
+        {
+            //Example 1
+            var createPerson = new Person()
+            {
+                Title = "John Doe",
+                Body = "Title for John Doe",
+                UserId = 1
+            };
+            var response = await httpClient.PostAsJsonAsync(url, createPerson);
+            if (response.IsSuccessStatusCode)
+            {
+                var person = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Person created: {person}");
+            }
+            var people = JsonSerializer.Deserialize<List<Person>>(await httpClient.GetStringAsync(url), jsonSerializerOptions);
+            
+            Console.ReadKey();
+        }
+    }
+}
