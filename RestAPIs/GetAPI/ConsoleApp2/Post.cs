b4424fa6d2a59a5f46d1d98ab05a876fce280660
Guilherme Soares <guicesarsosil@gmail.com>
2025-07-03 11:05:00 -0300

    Add GET example with JsonSerializer and LINQ filtering for userId

---
+++ RestAPIs/GetAPI/ConsoleApp2/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace PostClass;
+
+public class Post
+{
+    
+    public int UserId  { get; set; }
+    public int Id  { get; set; }
+    public string Title  { get; set; }
+    public string Body  { get; set; }
+}
---
+++ RestAPIs/GetAPI/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using PostClass;
+using System.Net.Http;
+using System.Text.Json;
+using System.Linq;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            var response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts");
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Response is OK (200)");
+                var json = await response.Content.ReadAsStringAsync();
+                // Console.WriteLine(json);
+                
+                var jsonSerializerOptions = new JsonSerializerOptions {PropertyNameCaseInsensitive = true};
+                
+                // Deserialize json and convert it to List 
+                var post = JsonSerializer.Deserialize<List<Post>>(json, jsonSerializerOptions); 
+                
+                var filterPost = post.Where(x => x.UserId == 1);
+                
+                foreach (var item in filterPost)
+                {
+                    Console.WriteLine($"UserId = {item.UserId}, Id = {item.Id}, Title = {item.Title}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+    }
+}
