a7a91a34b257978a3d27a31a8730c51e4d2f7749
Guilherme Soares <guicesarsosil@gmail.com>
2025-07-05 16:48:48 -0300

    Fetch users and filter by city using LINQ

---
+++ RestAPIs/GetAPI/ConsoleApp3/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+    Exercise 1 – GET + LINQ Filter
+    Goal: Fetch users and display only those who live in a specific city.
+
+    Endpoint: https://jsonplaceholder.typicode.com/users
+
+    Task:
+
+    Deserialize the JSON into a list of user objects.
+
+    Filter users where address.city == "South Christy".
+
+    Print their name, email, and city.
+
+*/
+
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Collections.Generic;
+using UserNamespace;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var url = "https://jsonplaceholder.typicode.com/users";
+
+        using HttpClient client = new HttpClient();
+        {
+            var response = await client.GetAsync(url); // Get all response in URL
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync(); // Read content as String
+
+                var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+                var users = JsonSerializer.Deserialize<List<Users>>(json, options); 
+
+                var filterUser = users
+                .Where(x => x.Address.City == "South Christy" && x.Address != null)
+                .ToList();
+
+                foreach (var data in filterUser)
+                {
+                    Console.WriteLine($"Name: {data.Name}, Email: {data.Email}, City: {data.Address.City}"); 
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+           
+        }
+    }
+}
---
+++ RestAPIs/GetAPI/ConsoleApp3/Users.cs
@@ -0,0 +1,13 @@
+namespace UserNamespace;
+
+class Users
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public Address Address { get; set; }
+}
+
+class Address
+{
+    public string City { get; set; }
+}
