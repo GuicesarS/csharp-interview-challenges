3b28bc09a0bb920306c3a38a008b510f09d9c93d
guicesars <guicesarsosil@gmail.com>
2025-07-16 21:39:42 -0300

    feat: Add EmailDomainCounter example using Dictionary and Split
    - Added a C# console app to count how many emails belong to each domain.
    - Uses Split to separate the list by ',' and then '@' for each email.
    - Stores domain counts in a Dictionary<string, int>.
    - Prints result showing each domain and total count.

---
+++ DataStructures/Dictionary/CountDomainEmail/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = "user1@gmail.com,user2@hotmail.com,user3@gmail.com,user4@yahoo.com";
+        string[] emails = input.Split(',');
+
+        Dictionary<string, int> Count = new Dictionary<string, int>();
+
+        foreach (string email in emails)
+        {
+            string[] parts = email.Split('@');
+            string domain = parts[1]; // parts[0] = username
+
+            if (Count.ContainsKey(domain))
+            {
+                Count[domain]++;
+            }
+            else
+            {
+                Count[domain] = 1;
+            }
+        }
+
+        foreach (var result in Count)
+        {
+            Console.WriteLine($"Domain: {result.Key} = {result.Value}");
+        }
+
+        Console.ReadKey();
+
+    }
+}
