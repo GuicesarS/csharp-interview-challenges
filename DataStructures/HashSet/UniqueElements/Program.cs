6edbe0819c2fe5a0991726e9e7fe84e48d467b98
guicesars <guicesarsosil@gmail.com>
2025-07-21 18:46:04 -0300

    feat: Add ElementsOnlyInA example using HashSet.ExceptWith

---
+++ DataStructures/HashSet/UniqueElements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> listA = new List<int> { 1, 2, 3, 4, 5 };
+        List<int> listB = new List<int> { 4, 5, 6, 7, 8 };
+
+        HashSet<int> setA = new HashSet<int>(listA);
+        HashSet<int> setB = new HashSet<int>(listB);
+
+        setA.ExceptWith(setB);
+
+        foreach (var sets in setA)
+        {
+            Console.Write(sets);
+        }
+          
+        Console.ReadKey();
+
+    }
+}
